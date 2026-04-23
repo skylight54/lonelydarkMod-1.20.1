1540f173563f4bfb12bdd31235c77b864d31c9f0
skylight54 <3552153440@qq.com>
2026-04-23 14:31:39 +0800

    1

---
+++ src/client/java/com/skylight/lonelydark/LonelydarkModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.skylight.lonelydark;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LonelydarkModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
